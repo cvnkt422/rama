4fa085084770f8254b56ddaae268c1a989900d39
cvnkt422 <cvnkt422@gmail.com>
2023-06-10 13:25:15 +0530

    secondCommit3

--- src/main/java/com/vnkt/rama/RamaApplication.java
+++ src/main/java/com/vnkt/rama/RamaApplication.java
@@ -36,7 +36,7 @@ public class RamaApplication {
 	@GetMapping
 	public ResponseEntity<String> showHome(){
 
-		logger.info("Coming inside Controller VNKT");
+		logger.infoS("Coming inside Controller VNKT");
 
 		return new ResponseEntity<>("Welcome Sri Rama Chandra Prabhu", HttpStatus.OK);
 
